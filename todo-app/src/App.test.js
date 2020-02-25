b3ff0cb1c127c222b2b8d26d0c1f37e272d56ac8
sumanth <sumanth712bs@gmail.com>
2020-02-25 10:27:26 +0100

    [ADD] Added test cases for the App.js

--- todo-app/src/App.test.js
+++ todo-app/src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {shallow} from "enzyme";
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Tests', () => {
+
+    it('should not crash when loaded', () => {
+        const wrapper = shallow(<App/>);
+        expect(wrapper).toHaveLength(1);
+    });
+
+    it('should have H1 tag when render', function () {
+        const wrapper = shallow(<App/>);
+        expect(wrapper.find('h1')).toHaveLength(1);
+        expect(wrapper.find('h1').text()).toEqual(' My Daily To-Do List ');
+    });
+
+    it('should have Table when renders', function () {
+        const wrapper = shallow(<App/>);
+        expect(wrapper.find('table')).toHaveLength(1);
+    });
 });
